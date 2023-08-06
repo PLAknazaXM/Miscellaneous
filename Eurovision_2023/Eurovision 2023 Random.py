f1e3de7be0187bdf39a44905cfc692029bf3677c
Kyle N <123481268+PLAknazaXM@users.noreply.github.com>
2023-08-06 11:28:22 -0400

    Add files via upload

---
+++ Eurovision_2023/Eurovision 2023 Random.py
@@ -0,0 +1,81 @@
+import random
+import webbrowser
+
+song = random.randrange(1,38,1)
+
+print(song)
+
+if song == 1:
+    webbrowser.open_new('https://youtu.be/mp8OG4ApocI') # Albania
+elif song == 2:
+    webbrowser.open_new('https://youtu.be/Co8ZJIejXBA') # Armenia
+elif song == 3:
+    webbrowser.open_new('https://youtu.be/aqtu2GspT80') # Australia
+elif song == 4:
+    webbrowser.open_new('https://youtu.be/ZMmLeV47Au4') # Austria
+elif song == 5:
+    webbrowser.open_new('https://youtu.be/5dvsr-L3HgY') # Azerbaijan
+elif song == 6:
+    webbrowser.open_new('https://youtu.be/ORhEoS6d8e4') # Belgium
+elif song == 7:
+    webbrowser.open_new('https://youtu.be/AyKj8jA0Qoc') # Croatia
+elif song == 8:
+    webbrowser.open_new('https://youtu.be/zrFUKqTy4zI') # Cyprus
+elif song == 9:
+    webbrowser.open_new('https://youtu.be/bFm-hw2rUeA') # Czechia
+elif song == 10:
+    webbrowser.open_new('https://youtu.be/04C8E7PUMQo') # Denmark
+elif song == 11:
+    webbrowser.open_new('https://youtu.be/wO9g5t3VSuw') # Estonia
+elif song == 12:
+    webbrowser.open_new('https://youtu.be/rNrgQm5z07U') # Finland
+elif song == 13:
+    webbrowser.open_new('https://youtu.be/GWfbEFH9NvQ') # France
+elif song == 14:
+    webbrowser.open_new('https://youtu.be/E8kO-QPippo') # Georgia
+elif song == 15:
+    webbrowser.open_new('https://youtu.be/5I9CYu668jA') # Germany
+elif song == 16:
+    webbrowser.open_new('https://youtu.be/qL0EkId_sTY') # Greece
+elif song == 17:
+    webbrowser.open_new('https://youtu.be/BhlJXcCv7gw') # Iceland
+elif song == 18:
+    webbrowser.open_new('https://youtu.be/ak5Fevs424Y') # Ireland
+elif song == 19:
+    webbrowser.open_new('https://youtu.be/r4wbdKmM3bQ') # Israel
+elif song == 20:
+    webbrowser.open_new('https://youtu.be/_iS4STWKSvk') # Italy
+elif song == 21:
+    webbrowser.open_new('https://youtu.be/xAjVA3qwqGk') # Latvia
+elif song == 22:
+    webbrowser.open_new('https://youtu.be/68lbEUDuWUQ') # Lithuania
+elif song == 23:
+    webbrowser.open_new('https://youtu.be/Apqwl0ayL6A') # Malta
+elif song == 24:
+    webbrowser.open_new('https://youtu.be/jatc6dNgpjs') # Moldova
+elif song == 25:
+    webbrowser.open_new('https://youtu.be/UOf-oKDlO6A') # Netherlands
+elif song == 26:
+    webbrowser.open_new('https://youtu.be/vSfffjHjdTk') # Norway
+elif song == 27:
+    webbrowser.open_new('https://youtu.be/ANM4CwbE0Is') # Poland
+elif song == 28:
+    webbrowser.open_new('https://youtu.be/-uY37gGPkNU') # Portugal
+elif song == 29:
+    webbrowser.open_new('https://youtu.be/NRxv-AUCinQ') # Romania
+elif song == 30:
+    webbrowser.open_new('https://youtu.be/Hjfq-T-8WHw') # San Marino
+elif song == 31:
+    webbrowser.open_new('https://youtu.be/oeIVwYUge8o') # Serbia
+elif song == 32:
+    webbrowser.open_new('https://youtu.be/zDBSIGITdY4') # Slovenia
+elif song == 33:
+    webbrowser.open_new('https://youtu.be/NGnEoSypBhE') # Spain
+elif song == 34:
+    webbrowser.open_new('https://youtu.be/b3vJfR81xO0') # Sweden
+elif song == 35:
+    webbrowser.open_new('https://youtu.be/_8-Sbc_GZMc') # Switzerland
+elif song == 36:
+    webbrowser.open_new('https://youtu.be/neIscK1hNxs') # Ukraine
+elif song == 37:
+    webbrowser.open_new('https://youtu.be/tJ21grjN6wU') # United Kingdom
